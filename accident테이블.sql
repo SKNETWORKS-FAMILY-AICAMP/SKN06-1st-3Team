d6e2b18155c11992c7e26bd3e7389970cdfd9a28
Park ChangGyu <qkrckdrb4106@gmail.com>
2024-10-11 16:27:06 +0900

    accident

--- accident테이블.sql
+++ accident테이블.sql
@@ -1,5 +1,9 @@
+create database accident;
+
+USE accident;
+
 CREATE TABLE ACCIDENT(
-	accident_idx INT PRIMARY KEY,
+	accident_idx INT AUTO_INCREMENT PRIMARY KEY,
     si_idx INT,
     gungu_idx INT,
     death_idx INT,
@@ -8,8 +12,20 @@ CREATE TABLE ACCIDENT(
     city_county INT,
     high_speed_national_highway INT,
     etc INT,
-    CONSTRAINT pk_accident PRIMARY KEY(accident_idx),
     CONSTRAINT fk_si_dix foreign key(si_idx) references siTable(si_idx) ON DELETE CASCADE,
     CONSTRAINT fk_gungu_idx foreign key(gungu_idx) references gunguTable(gungu_idx) ON DELETE CASCADE,
     CONSTRAINT fk_death_idx foreign key(death_idx) references deathTable(death_idx) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Category(
+	category_idx INT AUTO_INCREMENT PRIMARY KEY,
+    classification VARCHAR(100)
+);
+CREATE TABLE FAQ(
+	fa_idx INT AUTO_INCREMENT PRIMARY KEY,
+    category_idx INT,
+    title VARCHAR(100),
+    content VARCHAR(255),
+    CONSTRAINT fk_category_idx foreign key(category_idx) references Category(category_idx) ON DELETE CASCADE
 );
