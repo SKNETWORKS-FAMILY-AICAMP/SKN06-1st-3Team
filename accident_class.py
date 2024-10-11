31fb2b753083d2a8d68c66f0f50bca78c590d928
eunjinn <eunjinn95@gmail.com>
2024-10-11 19:16:11 +0900

    작업중 공유

--- accident_class.py
+++ accident_class.py
@@ -3,26 +3,26 @@ import pymysql
 import pymysql.cursors
 
 class CARDao:
-    def __init__(self, host:str, port:int, user:str, password:str, db:str):
-        self.host = host
-        self.port = port
-        self.user = user
-        self.password = password
-        self.db=db
+    def __init__(_self, host:str, port:int, user:str, password:str, db:str):
+        _self.host = host
+        _self.port = port
+        _self.user = user
+        _self.password = password
+        _self.db=db
         
 
-    def get_connection(self) -> pymysql.Connection:
-        return pymysql.connect(host=self.host, port=self.port, user=self.user, password=self.password, db=self.db)
+    def get_connection(_self) -> pymysql.Connection:
+        return pymysql.connect(host=_self.host, port=_self.port, user=_self.user, password=_self.password, db=_self.db)
     
-    def select_job(self):
-        """
-        업무 조회 메소드
-        return tuple[job_id, job_name]
-        """
-        sql = "SELECT job_id, job_title FROM job ORDER BY job_title"
-        with self.get_connection() as conn:
+    def insert_accident_data(_self):
+        return True
+
+
+    def insert_sido (_self, data) :
+        sql = "INSERT INTO sido (location) VALUES (%s)"
+        with _self.get_connection() as conn:
             with conn.cursor() as cursor:
-                cursor.execute(sql)
-                return cursor.fetchall()
-
-
+                cnt = cursor.executemany(sql, data)  # executemany() 결과 행수 반환.
+                conn.commit()
+        
+        print("insert된 총 행수:", cnt)
