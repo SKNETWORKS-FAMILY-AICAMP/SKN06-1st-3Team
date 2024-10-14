c9fb08fb33f15a2e7df72588f079fbf8ffdf09e9
eunjinn <eunjinn95@gmail.com>
2024-10-14 12:05:56 +0900

    [PYTHON] 사고 데이터 완료

--- accident_class.py
+++ accident_class.py
@@ -15,36 +15,60 @@ class CARDao:
         return pymysql.connect(host=_self.host, port=_self.port, user=_self.user, password=_self.password, db=_self.db)
         
     def insert_accident_data(_self, data):
-        insert_sql = """insert into accident (accident_idx, si_idx, sigungu_name, death_idx, national_road_province, special_metropolitan_city, 
+        insert_sql = """insert into accident (si_idx, sigungu_idx, death_idx, normal_road, national_road_province, special_metropolitan_city, 
         city_county, high_speed_national_highway, etc) values(%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
         with _self.get_connection() as conn:
             with conn.cursor() as cursor:
                 cursor.executemany(insert_sql, data)
                 conn.commit()
         print("사고 수:", len(data))
-        print("삽입된 값들:", data)
 
 
-    def insert_sido_gungu (_self, data) :
+    def insert_sido (_self, data) :
         sql = "INSERT INTO sido (location) VALUES (%s)"
         with _self.get_connection() as conn:
             with conn.cursor() as cursor:
                 cnt = cursor.executemany(sql, data)  # executemany() 결과 행수 반환.
                 conn.commit()
-        
-        print("insert된 총 행수:", cnt)
 
     def insert_sigungu(_self, data) :
-        f_sql = "SELECT si_idx FROM sido WHERE location = %s"
-        sql = "INSERT INTO sigungu (sigungu_name, si_idx) VALUES (%s, %s)"
+        sql = "INSERT INTO sigungu (si_idx, sigungu_name) VALUES (%s, %s)"
         with _self.get_connection() as conn:
             with conn.cursor() as cursor:
-                for i in data :
-                    cursor.execute(f_sql, i[0])
-                    si_idx = cursor.fetchone()[0]
-                    
-                    arr_data = [i[1], si_idx]
-                    cursor.execute(sql, arr_data)
-                    conn.commit()
+                cursor.execute(sql, data)
+                conn.commit()
                 
+    def insert_death_type(_self, data) :
+        sql = "INSERT INTO death_type (acc_level) VALUES (%s)"
+        with _self.get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.executemany(sql, data)
+                conn.commit()
+        
+
+    def select_si(_self, location) :
+        f_sql = "SELECT si_idx FROM sido WHERE location = %s"
+        with _self.get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f_sql, location)
+                si_idx = cursor.fetchone()
+        return si_idx
+
+
+    def select_sigungu(_self, location) :
+        f_sql = "SELECT sigungu_idx FROM sigungu WHERE sigungu_name = %s"
+        with _self.get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f_sql, location)
+                sigungu_idx = cursor.fetchone()
+        return sigungu_idx
+
+
+    def select_death_type(_self, acc_level) :
+        f_sql = "SELECT d_idx FROM death_type WHERE acc_level = %s"
+        with _self.get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(f_sql, acc_level)
+                d_idx = cursor.fetchone()
+        return d_idx
         
