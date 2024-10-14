f4c9a30458057463120e9ce6e7b8ceacdd4f08f7
Park ChangGyu <qkrckdrb4106@gmail.com>
2024-10-15 08:32:32 +0900

    [SQL] Query 수정

--- accident테이블.sql
+++ accident테이블.sql
@@ -5,27 +5,29 @@ USE accident;
 CREATE TABLE ACCIDENT(
 	accident_idx INT AUTO_INCREMENT PRIMARY KEY,
     si_idx INT,
-    sigungu_name INT,
+    sigungu_idx INT,
     death_idx INT,
-    national_road_province INT,
-    special_metropolitan_city INT,
-    city_county INT,
-    high_speed_national_highway INT,
-    etc INT,
+    national_road_province varchar(10),
+    special_metropolitan_city varchar(10),
+    city_county varchar(10),
+    high_speed_national_highway varchar(10),
+    etc varchar(10),
+    normal_load varchar(10),
     CONSTRAINT fk_si_dix foreign key(si_idx) references sido(si_idx) ON DELETE CASCADE,
-    CONSTRAINT fk_sigungu_name foreign key(sigungu_name) references sigungu(sigungu_idx) ON DELETE CASCADE,
+    CONSTRAINT fk_sigungu_name foreign key(sigungu_idx) references sigungu(sigungu_idx) ON DELETE CASCADE,
     CONSTRAINT fk_death_idx foreign key(death_idx) references death_type(d_idx) ON DELETE CASCADE
 );
 
 
 CREATE TABLE Category(
-    category VARCHAR(100) PRIMARY KEY
+	category_idx INT auto_increment PRIMARY KEY,
+    category VARCHAR(100) UNIQUE
 );
 
 CREATE TABLE FAQ(
 	fa_idx INT AUTO_INCREMENT PRIMARY KEY,
-    category varchar(100),
-    title VARCHAR(255),
+    category_idx INT,
+    title VARCHAR(255), 	
     content TEXT,
-    CONSTRAINT fk_category foreign key(category) references Category(category) ON DELETE CASCADE
+    CONSTRAINT fk_category_idx foreign key(category_idx) references Category(category_idx) ON DELETE CASCADE
 );
